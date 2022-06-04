b7c721ec12ea730bfab6d23918798a07b5674d70
jorge carlotto <jorgemcarlotto@gmail.com>
2022-06-04 20:49:45 -0300

    server and conect ok

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const routerMain = require('./routers/main.js');
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.use('/',routerMain);
+
+app.listen(3000, ()=>console.log('Server running...'+"http://localhost:3001"))
