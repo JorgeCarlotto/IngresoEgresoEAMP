0c55ed26d35fb09ad235ca760dd25c91efabd7bf
JorgeCarlotto <jorgemcarlotto@gmail.com>
2022-06-08 15:35:16 -0300

    css ok,logica de fecha y tiempo test ok

--- controller/mainControler.js
+++ controller/mainControler.js
@@ -1,8 +1,46 @@
 let mainController ={
 
     index: function(req,res){
-        res.render('main')
+
+        setInterval(() => {
+            
+        }, );
+        let fecha = new Date()
+        let semana=["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"]
+        let mes =["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
+        let diaNuevo;
+        let estado;
+
+        if(fecha.getHours() >= 12){
+            estado ="PM"
+        }else{
+            estado ="AM"
+        }
+
+
+        if (fecha.getDate()<10){
+            diaNuevo = `0${fecha.getDate()}`
+        }else
+            diaNuevo = `${fecha.getDate()}`
+
+        hora = fecha.toLocaleTimeString(),
+
+
+        res.render('main',
+        {
+            diaCompleto:semana[fecha.getDay()] +" "+ diaNuevo + " "+mes[fecha.getMonth()]+ " " + "de " +fecha.getFullYear(),
+            estado:estado,
+            horaCompleta: hora,
+
+
+        }
+    )
+
     },
 }
 
+
+
 module.exports = mainController;
+
+
